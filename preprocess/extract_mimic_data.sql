54d73d79ca857b11b3a41e9421ad18b6da4fa733
Bensoncasey7 <bensoncasey7@gmail.com>
2022-04-12 21:29:26 -0500

    Strip out bad pieces of sql generator

--- preprocess/extract_mimic_data.sql
+++ preprocess/extract_mimic_data.sql
@@ -1,4 +1,5 @@
 #create master MIMIC event table
+drop table if exists CS3750_Group2.KY_MIMIC_EVENTS_V4;
 CREATE TABLE CS3750_Group2.KY_MIMIC_EVENTS_V4 as
 select
 HADM_ID,
@@ -13,39 +14,6 @@ case when adm.DEATHTIME is not null then 1 else 0 end as ITEMID2,
 adm.ADMITTIME as EventStartTime,
 timestampdiff(hour,adm.DISCHTIME,adm.DISCHTIME) as Time_to_Discharge
 from mimiciiiv14.ADMISSIONS adm
-where adm.DBSOURCE = 'metavision'
-#and adm.HADM_ID = 105017
-
-union all
-
-select
-sep.HADM_ID,
-'Sepsis' as EventType,
-1 as ITEMID,
-1 as ITEMID2,
-sep.STARTTIME as EventStartTime,
-timestampdiff(hour,adm.DISCHTIME,sep.STARTTIME) as Time_to_Discharge
-from mimiciiiv14.SEPSIS_TABLE as sep
-join mimiciiiv14.ADMISSIONS adm on adm.HADM_ID = sep.HADM_ID
-where adm.DBSOURCE = 'metavision'
-#and adm.HADM_ID = 105017
-
-union all
-
-select
-aki.HADM_ID,
-'AKI' as EventType,
-AKI_LEVEL as ITEMID,
-AKI_LEVEL as ITEMID2,
-aki.CHARTTIME as EventStartTime,
-timestampdiff(hour,adm.DISCHTIME, aki.CHARTTIME) as Time_to_Discharge
-from (
-select
-HADM_ID,AKI_LEVEL,min(CHARTTIME) CHARTTIME
-from mimiciiiv14.AKI_TABLE
-group by HADM_ID,AKI_LEVEL) aki
-join mimiciiiv14.ADMISSIONS adm on adm.HADM_ID = aki.HADM_ID
-where adm.DBSOURCE = 'metavision'
 #and adm.HADM_ID = 105017
 
 union all
@@ -61,7 +29,7 @@ lab.CHARTTIME as EventStartTime,
 timestampdiff(hour,adm.DISCHTIME,lab.CHARTTIME) as Time_to_Discharge
 from mimiciiiv14.ADMISSIONS adm
 left join mimiciiiv14.LABEVENTS lab on adm.HADM_ID = lab.HADM_ID
-where adm.DBSOURCE = 'metavision' and lab.ITEMID is not null
+where lab.ITEMID is not null
 #and adm.HADM_ID = 105017
 
 union all
@@ -72,7 +40,7 @@ select adm.HADM_ID,
 timestampdiff(hour, adm.DISCHTIME, med.STARTTIME) as Time_to_Discharge
 from mimiciiiv14.ADMISSIONS adm
 left join mimiciiiv14.INPUTEVENTS_MV med on adm.HADM_ID = med.HADM_ID
-where adm.DBSOURCE = 'metavision' and med.ITEMID is not null
+where  med.ITEMID is not null
 #and adm.HADM_ID = 105017
 
 union all
@@ -88,7 +56,7 @@ vit.CHARTTIME as EventStartTime,
 timestampdiff(hour, adm.DISCHTIME, vit.CHARTTIME) as Time_to_Discharge
 from mimiciiiv14.ADMISSIONS adm
 left join mimiciiiv14.CHARTEVENTS vit on adm.HADM_ID = vit.HADM_ID
-where adm.DBSOURCE = 'metavision' and vit.ITEMID is not null
+where vit.ITEMID is not null
 #and adm.HADM_ID = 105017
     ) T
 order by HADM_ID,Time_to_Discharge
@@ -98,7 +66,7 @@ order by HADM_ID,Time_to_Discharge
 
 #create admission length table
 set @row_number = 0;
-drop table CS3750_Group2.KY_ADM_LENGTH;
+drop table if exists CS3750_Group2.KY_ADM_LENGTH;
 create table CS3750_Group2.KY_ADM_LENGTH as
 select (@row_number:=@row_number + 1) as row,
 t.* from
@@ -121,13 +89,11 @@ union all
 select 'Med' as EventType,l.ITEMID2, d.LABEL
 from (select distinct ITEMID,ITEMID2 from CS3750_Group2.KY_MIMIC_EVENTS_V4 where EventType = 'Med') l
 left join mimiciiiv14.D_ITEMS d on l.ITEMID = d.ITEMID
-where d.DBSOURCE = 'metavision'
 
 union all
 
 select 'Vit' as EventType,l.ITEMID2, d.LABEL
 from (select distinct ITEMID, ITEMID2 from CS3750_Group2.KY_MIMIC_EVENTS_V4 where EventType = 'Vit') l
 left join mimiciiiv14.D_ITEMS d on l.ITEMID = d.ITEMID
-where d.DBSOURCE = 'metavision'
 )
 t;
